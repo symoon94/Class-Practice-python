c634a8876fcdc6962d3590f82fdd895459fcc8f4
sooyoung-moon <vbfheu@naver.com>
2019-04-22 21:23:21 -0700

    add results

--- class_char1_reuse.py
+++ class_char1_reuse.py
@@ -12,11 +12,6 @@ human2 = Human()
 human1.first = 'Soo'
 human2.first = 'Tae'
 
-print(
-    "human1.first:", human1.first, "\n"
-    "human2.first:", human2.first
-)
-
 def Human():
     pass
 
@@ -26,7 +21,13 @@ fake2 = Human
 fake1.first = 'Monday'
 fake2.first = 'Tuesday'
 
-print(
-    "fake1.first:", fake1.first, "\n"
-    "fake2.first:", fake2.first
-)
+print(f"human1.first: {human1.first}\n"
+      f"human2.first: {human2.first}\n"
+      f"fake1.first: {fake1.first}\n"
+      f"fake2.first: {fake2.first}")
+
+# results >>>
+# human1.first: Soo
+# human2.first: Tae
+# fake1.first: Tuesday
+# fake2.first: Tuesday
